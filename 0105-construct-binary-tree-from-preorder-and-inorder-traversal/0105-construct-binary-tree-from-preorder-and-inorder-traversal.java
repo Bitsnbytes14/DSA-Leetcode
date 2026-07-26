960404911b4ce231177ed28bf54f49bc537400fe
Mohammad Ahmad <moahmad9632580741@gmail.com>
2026-07-26 14:36:02 +0530

    Time: 2 ms (64.77%), Space: 46.4 MB (38.09%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, preorder.length - 1,
+                     inorder, 0, inorder.length - 1, map);
+    }
+
+    private TreeNode build(int[] preorder, int preStart, int preEnd,
+                           int[] inorder, int inStart, int inEnd,
+                           Map<Integer, Integer> map) {
+
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        int index = map.get(root.val);
+        int leftSize = index - inStart;
+
+        root.left = build(preorder,
+                          preStart + 1,
+                          preStart + leftSize,
+                          inorder,
+                          inStart,
+                          index - 1,
+                          map);
+
+        root.right = build(preorder,
+                           preStart + leftSize + 1,
+                           preEnd,
+                           inorder,
+                           index + 1,
+                           inEnd,
+                           map);
+
+        return root;
+    }
+}
